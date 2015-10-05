5aa6c9a3744e019d55801554d1dbaddcf1cabbe4
kartaris <rust768@gmail.com>
2015-10-05 14:47:51 +0300

    Problem solved

---
+++ acmp.ru/0003_Game/0003_Game/Source.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a;
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	scanf("%d%d", &a);
+	printf("%d9%d", a, 9-a);
+	return 0;
+}
